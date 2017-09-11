55332b052a0e73f1ec9c01778ec0eb50c35955e9
Jim Maas <jimmaasuk@gmail.com>
2017-09-11 10:04:13 +0100

    compiles and runs but does not respond to arguments changed at call

--- R/jamfuncs.R
+++ R/jamfuncs.R
@@ -1,26 +1,32 @@
 #' Test function level 1
-#' @param var11 first variable for function 1
-#' @param var12 second variable for function 1
-#' @param var13 third variable for function 1
+#' @param num.sim first variable for function 1
+#' @param num.per second variable for function 1
+#' @param num.day third variable for function 1
 #' @export fun1
 
-fun1 <- function (var11=10, var12=8, var13=5,
+fun1 <- function (num.sim=10, num.per=8, num.day=5,
                   var21=0.05, var22=9.876,
-                  fun3on=TRUE, ...) {
+                  ...) {                  
 
-    results <- data.frame (foreach::`%dopar%`(
-            foreach::`%:%`(foreach::foreach(j = 1:var11,
+    final.results <- data.frame (foreach::`%dopar%`(
+            foreach::`%:%`(foreach::foreach(j = 1:num.sim,
                                             .combine = cbind,
                                             .packages="toymod2",
-                                            .export = "fun3on"),
-            foreach::foreach (i = 1:var12, .combine=rbind)),
-                   out3 <- replicate(var13, do.call(fun2, list(var21, var22)))
-        )
+                                            .export = c("fun2", "fun3")),
+                           foreach::foreach (i = 1:num.per, .combine=rbind)),
+            {
+            out3 <- replicate(num.day, eval(call("fun2")))
+            ##out3 <- replicate(num.day, do.call(fun2, list(var21, var22,
+            ##                                            fun3on = fun3on, ...)))
+            }
+            )
 )
 ## save outputs for subsequent analyses if required
-saveRDS(results, file = paste("./outputs/", var13 ,"_", var12, "_", var11, "_",
-                              format(Sys.time(), "%d_%m_%Y"), ".rds", sep=""))
-return(results)
+    saveRDS(final.results, file = paste("./outputs/", num.day ,"_",
+                                        num.per, "_", num.sim, "_",
+                                        format(Sys.time(), "%d_%m_%Y"),
+                                        ".rds", sep=""))
+return(final.results)
 }
 
 #' Test function level 2
@@ -45,5 +51,5 @@ fun3 <- function (var31=1.396, var32=14.387,
                   var33=3.219, fun3on = TRUE, ...) {
     out3 <- `if` (fun3on,
                   var31 * rnorm(1, mean=var32, sd= var33),
-                  500)
+                  0)
 }
---
+++ driver1.R
@@ -0,0 +1,23 @@
+rm(list=ls(all=TRUE))
+
+detach("package:toymod3", unload=TRUE)
+library(toymod3)
+
+## library(doParallel)
+## cl <-makeCluster(10)
+## registerDoParallel(cl)
+cl <- doMPI::startMPIcluster(count=10)
+doMPI::registerDoMPI(cl)
+##doMPI::exportDoMPI(cl, c('fun2', 'fun3'))
+
+(fun3(fun3on = TRUE))
+
+(fun1())
+
+(stuff1 <- replicate(10, mean(unlist(fun1()))))
+
+(stuff1 <- replicate(10, mean(unlist(fun1(fun3on=FALSE)))))
+
+doMPI::closeCluster(cl)
+
+q()
