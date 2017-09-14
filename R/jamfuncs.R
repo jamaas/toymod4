75b4c3af2611f6856843b28f50d3a99c15c10ae5
Jim Maas <jimmaasuk@gmail.com>
2017-09-14 08:41:43 +0100

    first commit, now have branching example working

--- R/jamfuncs.R
+++ R/jamfuncs.R
@@ -4,15 +4,20 @@
 #' @param num.day third variable for function 1
 #' @export fun1
 
-fun1 <- function (num.sim=10, num.per=8, num.day=5,...) {
+fun1 <- function (fun2.params, fun31.params, fun32.params, num.sim=10,
+                  num.per=8, num.day=5, ...) {
     final.results <- data.frame (foreach::`%dopar%`(
         foreach::`%:%`(foreach::foreach(j = 1:num.sim,
                                         .combine = cbind,
-                                        .export = c (ls(globalenv())),
-                                        .packages= c("toymod3")),
+                                        .packages= "toymod3"),
                        foreach::foreach (i = 1:num.per, .combine=rbind)),
         {
-            out3 <- replicate(num.day, eval(call("fun2")))
+            out3 <- replicate(num.day,
+                              do.call(fun2,
+                                      c(list(fun31.params=fun31.params,
+                                             fun32.params=fun32.params),
+                                             fun2.params)))
+            out2 <- data.frame(mean(out3))
         }
     )
     )
@@ -27,10 +32,13 @@ fun1 <- function (num.sim=10, num.per=8, num.day=5,...) {
 #' Test function level 2
 #' @param var21 first variable for function 2
 #' @param var22 second variable for function 2
+#' @param fun32on turn this copy of fun3 on or off
 #' @export fun2
 
-fun2 <- function (var21=0.5, var22=5, ...) {
-    out2 <- ifelse (rpois(1, var21) > 0, var22 * fun3(...), 0)
+fun2 <- function (fun31.params, fun32.params, var21=0.5, var22=5, fun32on = TRUE, ...) {
+    out21 <- ifelse (rpois(1, var21) > 0, var22 * do.call(fun3, fun31.params), 0)
+    out22 <- ifelse (fun32on, do.call(fun3, fun32.params), 0 )
+    out2 <- out21 + out22
 }
 
 #' Test function level 3
@@ -38,7 +46,7 @@ fun2 <- function (var21=0.5, var22=5, ...) {
 #' @param fun3on turn the formula on or off
 #' @export fun3
 
-fun3 <- function (...) {
-    out3 <- ifelse (fun3on==TRUE, var31, 1)
+fun3 <- function (fun3on, var31, ...) {
+    out3 <- ifelse (fun3on, var31, 1)
 }
 
