62596d0c3142a3514348805f523d7a5a68c29bcc
Jim Maas <jimmaasuk@gmail.com>
2017-09-15 09:03:13 +0100

    updated passing of level 1 arguments

--- toymod4_driver1.R
+++ toymod4_driver1.R
@@ -12,27 +12,37 @@ fun31.params <- list(fun3on = TRUE, var31 = 500)
 fun32.params <- list(fun3on = TRUE, var31 = 183)
 
 ## run a first simulation with default values
-(fun1(fun2.params, fun31.params, fun32.params,
-      num.sim = 10, num.per = 8, num.day = 5))
+do.call(fun1,(c(list(fun31.params = fun31.params,
+                    fun32.params = fun32.params,
+                    fun2.params = fun2.params),
+               fun1.params)))
 
-## change a level 3 function value and see if it changes results
+## change a level 1 argument value and see if it changes
+fun1.params <- list(num.sim = 10, num.per = 20, num.day = 5)
+do.call(fun1,(c(list(fun31.params = fun31.params,
+                    fun32.params = fun32.params,
+                    fun2.params = fun2.params),
+               fun1.params)))
+
+
+## change a level 2 function value and see if it changes results
 fun2.params <- list(fun22on = FALSE, var21 = 0.5, var22 = 5)
-(fun1(fun2.params, fun31.params, fun32.params, num.sim = 10,
-      num.per = 8, num.day = 5))
+fun1.params <- list(num.sim = 10, num.per = 8, num.day = 5)
+do.call(fun1,(c(list(fun31.params = fun31.params,
+                    fun32.params = fun32.params,
+                    fun2.params = fun2.params),
+               fun1.params)))
+
 
 ## change a level 3 function value and see if it changes results
 fun2.params <- list(fun22on = TRUE, var21 = 0.5, var22 = 5)
 fun31.params <- list(fun3on = TRUE, var31 = 500)
 fun32.params <- list(fun3on = FALSE, var31 = 500)
-(fun1(fun2.params, fun31.params, fun32.params, num.sim = 10,
-      num.per = 8, num.day = 5))
+do.call(fun1,(c(list(fun31.params = fun31.params,
+                    fun32.params = fun32.params,
+                    fun2.params = fun2.params),
+               fun1.params)))
 
-## change a level 3 function value and see if it changes results
-fun2.params <- list(fun22on = TRUE, var21 = 0.5, var22 = 20)
-fun31.params <- list(fun3on = TRUE, var31 = 500)
-fun32.params <- list(fun3on = FALSE, var31 = 500)
-(fun1(fun2.params, fun31.params, fun32.params, num.sim = 10,
-      num.per = 8, num.day = 5))
 
 ## close down the cluster
 doMPI::closeCluster(cl)
