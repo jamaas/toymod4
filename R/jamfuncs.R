5aa929dd38e5e2d6531812a1864f4b1f326d2e20
Jim Maas <jimmaasuk@gmail.com>
2017-09-19 08:03:32 +0100

    first commit, error message not finding env e1

--- R/jamfuncs.R
+++ R/jamfuncs.R
@@ -2,29 +2,30 @@
 #' @param num.sim first variable for function 1
 #' @param num.per second variable for function 1
 #' @param num.day third variable for function 1
+#' @param fun2.params parameters for function 2
+#' @param fun31.params parameters for first call of function 3
+#' @param fun32.params parameters for second call of funtion 3
+#' @param fun4.params parameters for call to function 4
 #' @export fun1
-
 fun1 <- function (fun2.params, fun31.params, fun32.params, fun4.params,
                   num.sim=10, num.per=8, num.day=5, ...) {
     final.results <- data.frame (foreach::`%dopar%`(
         foreach::`%:%`(foreach::foreach(j = 1:num.sim,
                                         .combine = cbind,
-                                        .packages= "toymod4"),
-                       foreach::foreach (i = 1:num.per, .combine=rbind)),
-        {
-            out3 <- replicate(num.day,
-                              do.call(fun2,
-                                      c(list(fun31.params=fun31.params,
-                                             fun32.params=fun32.params,
-                                             fun4.params = fun4.params),
-                                             fun2.params)))
+                                        .packages= c("toymod4")),
+                       foreach::foreach (i = 1:num.per,
+                                         .packages = c("toymod4"),
+                                         .combine=rbind)), {
+            e1 <- new.env()
+            e1 <- list2env(c(fun2.params, fun31.params, fun32.params,
+                             fun4.params), e1)
+            out3 <- replicate(num.day, fun2(e1, var21, var22, fun22on))
             out2 <- data.frame(mean(out3))
         }
     )
     )
     ## save outputs for subsequent analyses if required
-    saveRDS(final.results, file = paste("./outputs/", num.day ,"_",
-                                        num.per, "_", num.sim, "_",
+    saveRDS(final.results, file = paste(num.day ,"_", num.per, "_", num.sim, "_",
                                         format(Sys.time(), "%d_%m_%Y"),
                                         ".rds", sep=""))
     return(final.results)
@@ -34,40 +35,37 @@ fun1 <- function (fun2.params, fun31.params, fun32.params, fun4.params,
 #' @param var21 first variable for function 2
 #' @param var22 second variable for function 2
 #' @param fun22on turn this copy of fun3 on or off
+#' @param env environment to get variables from 
 #' @export fun2
-
-fun2 <- function (fun31.params, fun32.params, fun4.params,
-                  var21, var22, fun22on, ...) {
-    out21 <- ifelse (rpois(1, var21) > 0,
-                     var22 * do.call(fun3, c(list(fun4.params=fun4.params),
-                                             fun31.params)),
-                     0)
-
-    out22 <- ifelse (fun22on,
-                     do.call(fun3, c(list(fun4.params=fun4.params),
-                                     fun32.params)),
-                     0)
-
+fun2 <- function (env, var21, var22, fun22on, ...) {
+    attach(env)
+    out21 <- ifelse (rpois(1, var21) > 0, var22 * fun3(e1, fun3on, var31), 0)
+    out22 <- ifelse (fun22on, fun3(e1, fun3on, var31), 0)
     out2 <- out21 + out22
+    detach(env)
+    out2
 }
 
 #' Test function level 3
 #' @param var31 first variable for function 3
 #' @param fun3on turn the formula on or off
 #' @export fun3
-
-fun3 <- function (fun3on, var31, fun4.params, ...) {
+fun3 <- function (env, fun3on, var31, ...) {
+    attach(env)
     out31 <- ifelse (fun3on, var31, 1)
-    out32 <- ifelse (fun3on, do.call(fun4, fun4.params), 0)
+    out32 <- ifelse (fun3on, fun4(e1, fun4on, var41), 0)
     out3 <- out31 + out32
+    detach(env)
+    out3
 }
 
 #' Test function level 4
 #' @param var41 first variable for function 4
 #' @param fun4on turn the formula on or off
 #' @export fun4
-
-fun4 <- function (fun4on, var41, ...) {
+fun4 <- function (env, fun4on, var41, ...) {
+    attach(env)
     out4 <- ifelse (fun4on, var41, 1)
+    detach(env)
+    out4
 }
-
--- toymod4_driver1.R
+++ toymod4_driver1.R
@@ -28,38 +28,5 @@ do.call(fun1,(c(list(fun4.params = fun4.params,
                      fun2.params = fun2.params),
                 fun1.params)))
 
-
-
-## change a level 2 function value and see if it changes results
-fun2.params <- list(fun22on = FALSE, var21 = 0.5, var22 = 5)
-fun1.params <- list(num.sim = 10, num.per = 8, num.day = 5)
-do.call(fun1,(c(list(fun4.params = fun4.params,
-                     fun31.params = fun31.params,
-                     fun32.params = fun32.params,
-                     fun2.params = fun2.params),
-                fun1.params)))
-
-
-## change a level 3 function value and see if it changes results
-fun2.params <- list(fun22on = TRUE, var21 = 0.5, var22 = 5)
-fun31.params <- list(fun3on = TRUE, var31 = 500)
-fun32.params <- list(fun3on = FALSE, var31 = 500)
-do.call(fun1,(c(list(fun4.params = fun4.params,
-                     fun31.params = fun31.params,
-                     fun32.params = fun32.params,
-                     fun2.params = fun2.params),
-                fun1.params)))
-
-## change a level 4 function value and see if it changes results
-fun32.params <- list(fun3on = TRUE, var31 = 500)
-fun4.params <- list(fun4on = FALSE, var41 = 36.1)
-
-do.call(fun1,(c(list(fun4.params = fun4.params,
-                     fun31.params = fun31.params,
-                     fun32.params = fun32.params,
-                     fun2.params = fun2.params),
-                fun1.params)))
-
-
 ## close down the cluster
 doMPI::closeCluster(cl)
